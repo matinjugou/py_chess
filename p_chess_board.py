ad8b68d0395c281bea55384c073f0fc15bbbab27
huangchao <837287450@qq.com>
2017-07-02 20:50:49 +0800

    add models and chess board

--- p_chess_board.py
+++ p_chess_board.py
@@ -1,1 +1,23 @@
-from PyQt5.QtWidgets import 
+from PyQt5.QtWidgets import QGraphicsPixmapItem
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class p_chessboard(QGraphicsPixmapItem):
+    pic = QPixmap()
+
+    placeChess = pyqtSignal(QPointF, name='')
+
+    def __init__(self, parent: QGraphicsPixmapItem = None):
+        super(p_chessboard, self).__init__()
+        pass
+
+    def __init__(self, str, parent: QGraphicsPixmapItem = None):
+        super(p_chessboard, self).__init__()
+        pic = QPixmap(str)
+        self.se
+        pass
+
+    def mousePressEvent(self, event):
+        self.placeChess.emit(event.pos())
+        pass
--- p_model.py
+++ p_model.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (QGraphicsObject, QGraphicsView, QGraphicsScene, QApplication)
 from PyQt5.Qt import QObject
-class p_model(QGraphicsScene):
+class p_multiple_model(QGraphicsScene):
     def __init__(self):
         QGraphicsScene.__init__(self)
         
