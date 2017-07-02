fc8556e23b4deaf3814ac7976579ceaa8e2ddf73
huangchao <837287450@qq.com>
2017-07-02 21:03:16 +0800

    add multiple mode

--- p_model.py
+++ p_model.py
@@ -1,6 +1,20 @@
 from PyQt5.QtWidgets import (QGraphicsObject, QGraphicsView, QGraphicsScene, QApplication)
 from PyQt5.Qt import QObject
-class p_multiple_model(QGraphicsScene):
-    def __init__(self):
-        QGraphicsScene.__init__(self)
-        
+from p_chess_board import p_chessboard
+from PyQt5.QtCore import *
+
+class p_multiple_model(QObject):
+
+    def __init__(self,parent:QObject = None):
+        super(p_multiple_model, self).__init__()
+        self.scene = QGraphicsScene()
+        self.chessboard = p_chessboard()
+
+        self.scene.addItem(self.chessboard)
+
+        self.chessboard.placeChess.connect(self.place_chess())
+        pass
+
+    def place_chess(self, pos:QPointF):
+
+        pass
