d5dd8a8da3b426af9b14b9185cafa80c3d7e3a38
huangchao <837287450@qq.com>
2017-07-02 18:21:51 +0800

    first push

---
+++ p_item.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QGraphicsPixmapItem
+from PyQt5.QtGui import QPixmap
+from abc import ABCMeta, abstractclassmethod
+
+class p_item(QGraphicsPixmapItem):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, parent:QGraphicsPixmapItem = None):
+        QGraphicsPixmapItem.__init__(self, parent)
+        pass
+
+    def __init__(self, pic:QPixmap, parent:QGraphicsPixmapItem = None):
+        QGraphicsPixmapItem.__init__(self,pic)
+        pass
